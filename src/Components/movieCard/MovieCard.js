b18c409846c38cee47772711540fa4eace754855
sunny <goodluck14115@gmail.com>
2023-03-10 11:08:25 +0500

    updated MovieCard

--- src/Components/movieCard/MovieCard.js
+++ src/Components/movieCard/MovieCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Styles from "./MovieCard.module.css";
 import { NavLink } from "react-router-dom";
-import ReactStars from "react-stars";
 
 const MovieCard = (props) => {
   const movieList = props.movies;
@@ -16,19 +15,6 @@ const MovieCard = (props) => {
     >
       <div className={Styles.movie_card}>
         <img src={`https://image.tmdb.org/t/p/original${e.poster_path}`} />
-        <h4 className={Styles.card_title}>{e.title}</h4>
-        <h4 className={Styles.rating}>
-          Rating:
-          <span>
-            <ReactStars
-              size={15}
-              half={true}
-              value={e.vote_average / 2}
-              edit={false}
-            />
-          </span>
-        </h4>
-        <h4>Year: {e.release_date}</h4>
       </div>
     </NavLink>
   ));
